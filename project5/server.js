eb3e9f92029c03cf7ae2130939d503dc84fbc826
Quantumdrill <quantumdrill317@gmail.com>
2025-04-24 03:50:05 -0400

    p5.v0

--- project5/server.js
+++ project5/server.js
@@ -18,8 +18,56 @@ let udb = new nedb({ filename: "userDatabase.txt", autoload: true })
 const nedbSession = nedbSessionStore({ connect: expressSession, filename: 'sessions.txt'})
 
 //middleware settings
-app.use(expressSession({
-    store: nedbSession,
-    cookie: {maxAge: 10**14},
-    secret: "youshouldntseethis"
-}))
+// app.use(expressSession({
+//     store: nedbSession,
+//     cookie: {maxAge: 10**14},
+//     secret: "youshouldntseethis"
+// }))
+app.set("view engine","ejs")
+app.use(express.static("static"))
+
+//global variables
+let accountPage = {title:"Login",type:"login"}
+let userData = {}
+
+app.get("/", (req,res)=>{
+    res.render("home.ejs")
+}) 
+
+app.get("/login", (req,res)=>{
+    accountPage = {title:"Login",type:"login"}
+    res.render("account.ejs",{account:accountPage})
+})
+
+app.get("/register", (req,res)=>{
+    accountPage = {title:"Register",type:"register"}
+    res.render("account.ejs",{account:accountPage})
+})
+
+app.post("/auth", (req,res)=>{
+    udb.findOne({userID: req.body.id}, (err,user)=>{
+        if (err||user === null){
+            //login not success
+        } else {
+            if (bcrypt.compareSync(req.body.password, user.password)){
+                res.redirect("/collections")
+            } else {
+                //login not success
+            }
+        }
+    })
+})
+
+app.post("/newAccount", (req,res)=>{
+    let newUser = {
+        userID: req.body.id,
+        password: bcrypt.hashSync(req.body.password,10)
+    }
+    udb.insert(newUser,(err,data)=>{
+        res.redirect("/collections")
+    })
+})
+
+app.listen(8081, ()=>{
+    console.log("http://127.0.0.1:8081")
+})
